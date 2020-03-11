73fcdcbabab0b6fecb4029be76f436326697b05f
Max <kolotseymax@gmail.com>
2020-03-11 10:11:28 +0300

    both companies and employees controllers are done, need to add a possibility to add companies to an employee

--- NTierApp.ASPMVC/Controllers/EmployeesController.cs
+++ NTierApp.ASPMVC/Controllers/EmployeesController.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
+using NTierApp.ASPMVC.Models;
+using NTierApp.ASPMVC.Models.Employee;
 using NTierApp.BLL.Interfaces;
+using NTierApp.BLL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +23,45 @@ namespace NTierApp.ASPMVC.Controllers
 
         public ActionResult Index()
         {
+            var employees = employeeService.GetEmployees();
+            return View(mapper.Map<List<EmployeesViewModel>>(employees));
+        }
+        [HttpGet]
+        public ActionResult Edit(long id)
+        {
+            var employee = employeeService.GetEmployee(id);
+            if (employee != null)
+                return View(mapper.Map<EditEmployeeViewModel>(employee));
+            else return new HttpNotFoundResult();
+        }
+        [HttpPost]
+        public ActionResult Edit(EditEmployeeViewModel employee)
+        {
+            employeeService.UpdateEmployee(mapper.Map<EmployeeBLL>(employee));
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(long id)
+        {
+            employeeService.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(long id)
+        {
+            var employee = employeeService.GetEmployee(id);
+            if (employee != null)
+                return View(mapper.Map<DetailsEmployeeViewModel>(employee));
+            else return new HttpNotFoundResult();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CreateEmployeeViewModel employee)
+        {
+            employeeService.AddEmployee(mapper.Map<EmployeeBLL>(employee));
+            return RedirectToAction("Index");
         }
     }
 }
